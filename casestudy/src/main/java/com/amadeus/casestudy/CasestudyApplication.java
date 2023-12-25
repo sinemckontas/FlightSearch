fab557d62ae57bf9fb087fa222f4d3d6f29880cf
Sinem C. Kontaş <sinemkontas@gmail.com>
2023-12-25 21:29:03 +0300

    Initial Commit

---
+++ casestudy/src/main/java/com/amadeus/casestudy/CasestudyApplication.java
@@ -0,0 +1,14 @@
+package com.amadeus.casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class CasestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasestudyApplication.class, args);
+	}
+
+}
---
+++ casestudy/src/test/java/com/amadeus/casestudy/CasestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amadeus.casestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
